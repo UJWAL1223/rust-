bb2de38571a5aa069ce77190243b5e3a6d726bd0
UJWAL1223 <141144687+UJWAL1223@users.noreply.github.com>
2025-03-01 21:00:06 +0545

    Delete thebook/variables/target directory

--- thebook/variables/target/debug/deps/variables-dc19508b7b097589.d
+++
@@ -1,5 +0,0 @@
-/home/teets/Documents/test/rust-/thebook/variables/target/debug/deps/variables-dc19508b7b097589: src/main.rs
-
-/home/teets/Documents/test/rust-/thebook/variables/target/debug/deps/variables-dc19508b7b097589.d: src/main.rs
-
-src/main.rs:
--- thebook/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/teets/Documents/test/rust-/thebook/variables/target/debug/variables: /home/teets/Documents/test/rust-/thebook/variables/src/main.rs

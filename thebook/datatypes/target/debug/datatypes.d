a3b66f5f34b84bf81ec73fe4f4a0f5458df792ae
UJWAL1223 <141144687+UJWAL1223@users.noreply.github.com>
2025-03-01 20:59:02 +0545

    Delete thebook/datatypes/target directory

--- thebook/datatypes/target/debug/datatypes.d
+++
@@ -1,1 +0,0 @@
-/home/teets/Documents/test/rust-/thebook/datatypes/target/debug/datatypes: /home/teets/Documents/test/rust-/thebook/datatypes/src/main.rs
--- thebook/datatypes/target/debug/deps/datatypes-17799b4a57881f4e.d
+++
@@ -1,5 +0,0 @@
-/home/teets/Documents/test/rust-/thebook/datatypes/target/debug/deps/datatypes-17799b4a57881f4e: src/main.rs
-
-/home/teets/Documents/test/rust-/thebook/datatypes/target/debug/deps/datatypes-17799b4a57881f4e.d: src/main.rs
-
-src/main.rs:
